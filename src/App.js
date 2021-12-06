60102006cbb3ded16c88c0e92a0f715c5825706a
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-12-06 23:27:18 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './Styles/App.css';
 import Header from './Components/Header/Header'
+import Services from './Components/Services/Services'
 
 function App() {
   return (
     <div>
       <Header />
+      <Services />
 
     </div>
   );
