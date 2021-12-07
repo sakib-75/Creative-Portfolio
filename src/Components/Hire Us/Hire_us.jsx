526b2dd2cf3ac5a2cb026fb00814e98285898e1d
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-12-07 20:08:11 +0600

    Create Hire_us.jsx

---
+++ src/Components/Hire Us/Hire_us.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../Hire Us/Hire-us.css'
+import Hireusimg from '../Images/Hire-us-img.png'
+import Button from '../Sub Components/Button'
+
+export default function Hire_us() {
+    return (
+        <section className="Hire-us-section">
+            <div className="container Hire-us-body">
+                <img src={Hireusimg} alt="" />
+                <div className="Hire-us-text">
+                    <h2 className="section-title">Why You Hire Us?</h2>
+                    <p className="paragraph">Proin ipsum arcu, lobortis id ullamcorper nec, vehicula eget magna. 
+                        Sed ut malesuada magna, eget lacinia sapien. Nam molestie non ex sit amet ornare. Nunc in pellentesque ipsum, vel vulputate sapien.
+                    </p>
+                    <p className="paragraph">Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. 
+                        Nam faucibus elementum arcu, eget eleifend tortor.
+                    </p>
+
+                    <Button button_title="Read More" />
+                </div>
+
+            </div>
+        </section>
+    );
+}
