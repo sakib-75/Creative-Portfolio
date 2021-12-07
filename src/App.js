f6f0ffcb3013be4cff8862a100f23446d13e9083
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-12-07 20:07:29 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './Styles/App.css';
+import './Styles/App.css'
 import Header from './Components/Header/Header'
 import Services from './Components/Services/Services'
+import HireUs from './Components/Hire Us/Hire_us'
+import Completedwork from './Components/Completed Work/Completed_work'
 
 function App() {
   return (
     <div>
       <Header />
       <Services />
+      <HireUs />
+      <Completedwork />
 
     </div>
   );
