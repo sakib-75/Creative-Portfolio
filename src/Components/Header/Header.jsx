cd0fb48ddbdeec8d1c729d8e26c884811aad03e4
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-12-26 23:29:24 +0600

    Mobile menu added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,8 @@ import '../Header/Header.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { FaBars } from "react-icons/fa";
+import { FaAlignLeft } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import Logo from '../Images/logo.png';
 import banner1 from '../Images/banner_1.png';
 import banner2 from '../Images/banner_02.png';
@@ -20,25 +21,40 @@ export default function Header() {
     slidesToScroll: 1
   };
 
+  const menuFunction = () => {
+    var x = document.querySelector(".main-nav");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  const menuFunctionOff = () => {
+    var x = document.querySelector(".main-nav");
+    x.style.display = "none";
+  }
+
+
   return (
     <header className="header-section">
       <div className="container">
         <div className="header-menu">
           <img src={Logo} alt="" />
           <nav className="main-nav">
+            <button className="menu-bar-off" onClick={menuFunctionOff}> <FaTimes /> </button>
             <ul className="nav-list">
               <li><a href="#about-us" className="nav-link">About us</a></li>
               <li><a href="#services" className="nav-link">Service</a></li>
               <li><a href="#portfolio" className="nav-link">Porlfolio</a></li>
               <li><a href="#pricing" className="nav-link">Pricing</a></li>
               <li><a href="#blog" className="nav-link">Blog</a></li>
-              <li><a href="#contact-us" className="nav-link">Contact us</a></li>
+                <li><a href="#contact-us" className="nav-link">Contact us</a></li>
             </ul>
-          </nav>
-          <div className="menu-btn">
             <Link_button button_title="Hire Us" link="#"/>
-            <button className="menu-bar"> <FaBars/> </button>
-          </div>
+          </nav>
+          
+          <button className="menu-bar" onClick={menuFunction}> <FaAlignLeft/> </button>
         </div>
         
         <Slider {...settings} className="hero-slider">
